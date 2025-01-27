ec46b51cdbcf92e874f763af71777c69a7d7d3b9
Julian Hammer <jh@realgolf.games>
2025-01-27 11:43:25 +0000

    Update health check endpoint and add .gitattributes for virtual environment

--- api/app.py
+++ api/app.py
@@ -2,9 +2,9 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-@app.route("/", methods=["GET"])
-def home():
-    return jsonify({"message": "Hello World!", "status": 200})
+@app.route("/health", methods=["GET"])
+def health():
+    return jsonify({"message": "Health Check passed", "status": 200})
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=3000)
